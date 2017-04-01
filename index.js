2d9c3b5f1e24c6580bad9e7c17440c6fa9ee42f7
Apple <apple@Apples-MacBook-Pro.local>
2017-04-01 15:04:23 +0700

    cookie

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 
 const app = express();
+app.use(cookieParser());
 app.listen(3000, () => console.log('Server started'));
+
+app.get('/getcookie', (req, res) => {
+    res.send(req.cookies);
+});
+
+app.get('/setcookie', (req, res) => {
+    res.cookie('name', 'Nguyen Van Pho');
+    res.send('Set successfully!');
+});
 
 app.get('/vaorap', (req, res) => {
     res.send('Welcome');
